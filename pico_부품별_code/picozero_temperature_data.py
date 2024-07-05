2556db3b0905340d3aab9d93e4263a3a39435e48
applepy <edh427@naver.com>
2024-07-05 12:18:10 +0900

    Update picozero_temperature_data.py

--- pico_부품별_code/picozero_temperature_data.py
+++ pico_부품별_code/picozero_temperature_data.py
@@ -8,8 +8,9 @@ print(start_time)
 
 dht_sensor = DHTSensor(pin=0, model='DHT11')
 
-f = open('temp001.csv', 'w') #피코 파일 경로랑 같이 있어야함
-f.write(f'Time,Temperature,Humidity\n')
+f = open('temp002.csv', 'w') #피코 파일 경로랑 같이 있어야함
+f.write(f'Time,Temperature,Humidity\n')#시간,분,초를 한줄에 입력받을 때
+f.write(f'Hour,Min,Second,Temperature,Humidity\n')#시간,분,초를 따로 입력 받을 때
 
 #1시간 측정을 기준
 while True:
@@ -22,8 +23,12 @@ while True:
     sleep(0.5)
 
     cur_time=localtime() #현재시각 측정
-    f.write(f'{cur_time[3]}:{cur_time[4]}:{cur_time[5]},{temp},{humi}\n')
+    #시간,분,초를 한줄로
+    #f.write(f'{cur_time[3]}:{cur_time[4]}:{cur_time[5]},{temp},{humi}\n')
+    #시간,분,초를 따로 입력받을 때
+    f.write(f'{cur_time[3]},{cur_time[4]},{cur_time[5]},{temp},{humi}\n')
 
 end_time=localtime() #측정 종료시간 확인
 f.write(f'{end_time}\n')
 f.close()
+
