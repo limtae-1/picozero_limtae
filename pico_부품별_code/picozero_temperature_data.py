56bf395ab4bfe8dfcba3ff435035af491ff36035
applepy <edh427@naver.com>
2024-07-05 11:42:58 +0900

    Update picozero_temperature_data.py

--- pico_부품별_code/picozero_temperature_data.py
+++ pico_부품별_code/picozero_temperature_data.py
@@ -3,31 +3,27 @@ from time import sleep, localtime
 
 #localtime() 의 튜플 값은
 #(연도, 달, 날, 시간, 분, 초, 요일[일~토/0~6], 1월1일부터 경과한 일 수)
-start_time=localtime()
+start_time=localtime() #측정 시작시간 확인
 print(start_time)
 
 dht_sensor = DHTSensor(pin=0, model='DHT11')
 
-f = open('test002.csv', 'w') #피코 파일 경로랑 같이 있어야함
-f.write(f'{start_time}\n')
+f = open('temp001.csv', 'w') #피코 파일 경로랑 같이 있어야함
+f.write(f'Time,Temperature,Humidity\n')
 
 #1시간 측정을 기준
-for i in range(3600):
+while True:
     temp=dht_sensor.temperature
     print(temp)
-    sleep(1)
+    sleep(0.5)
     
     humi = dht_sensor.humidity
     print(humi)
-    sleep(1)
+    sleep(0.5)
 
-    #real_time=localtime()
-    #f.write("%s" f'{real_time}\n' %data)
-    f.write("%d %d" %(temp,humi))
-    f.write("\n")
-    
+    cur_time=localtime() #현재시각 측정
+    f.write(f'{cur_time[3]}:{cur_time[4]}:{cur_time[5]},{temp},{humi}\n')
 
-end_time=localtime()
+end_time=localtime() #측정 종료시간 확인
 f.write(f'{end_time}\n')
 f.close()
-
