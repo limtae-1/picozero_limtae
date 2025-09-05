a5a1b0017ae948370eb155154e1b186017a085d1
applepy <edh427@naver.com>
2025-09-05 10:42:59 +0900

    Update picozero_I2CLCD.py

--- pico_부품별_code/picozero_I2CLCD.py
+++ pico_부품별_code/picozero_I2CLCD.py
@@ -5,6 +5,7 @@ from time import sleep
 # I2C LCD 디스플레이 통신속도 조절 가능 lcd = I2C_LCD(scl=5, sda=4, freq=1000000) 이처럼 파라미터에 freq 속성을 추가하면됨 굳이,, 조절할 필욘 없을듯)
 # LCD 디스플레이에 띄울 땐 문자형자료형으로 변환하여 띄우는 것에 주의할 것 !
 # 글씨가 깨져서 나오는 경우는 sleep()명령어를 통해 약간의 딜레이를 주면 해결됨
+# clear()명령어 앞에 sleep 붙이기
 lcd = I2C_LCD(scl=5, sda=4)
 
 # LCD 초기화
@@ -25,4 +26,5 @@ sleep(1)
 
 # 백라이트 켜기
 lcd.backlight_on()
+
 
